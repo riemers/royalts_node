a530c73edb7f6f05966e9510ba0d79ef679b6724
Erik-jan Riemers <erik-jan.riemers@ing.nl>
2015-05-19 11:40:13 +0200

    Added sample code

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    folder: function(name) {
+        console.log("folder, " + name);
+    },
+    terminal: function(name) {
+        console.log("terminal, " + name);
+    }
+};
