911ea2edcd94283fcd51a8cd592925c1de6d1181
Erik-jan Riemers <erik-jan.riemers@ing.nl>
2015-05-21 17:26:12 +0200

    First object block created

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+var builder = require('xmlbuilder');
+var uuid = require('node-uuid');
+
+function object(objectid,key,value) {
+    var xml = builder.create('KeyValuePairs')
+        .ele('ObjectID', objectid)
+            .up()
+        .ele('Key', key)
+            .up()
+        .ele('Value', value)
+        .end({pretty: true});
+    return (xml);
+}
+
 module.exports = {
+    init: function(name) {
+        return object(uuid.v4(),'Name','Pfinbox');
+        var folderid = uuid.v4();
+        var xml = builder.create('NewDataSet')
+            .ele('repo', {'type': 'guid'}, folderid)
+            .end({ pretty: true});
+        return(xml);
+    },
     folder: function(name) {
-        console.log("folder, " + name);
+        var folderid = uuid.v4();
+        console.log("folder troep2, " + name);
     },
     terminal: function(name) {
-        console.log("terminal, " + name);
+        var terminalid = uuid.v4();
+        console.log("terminal troep, " + name);
     }
 };
