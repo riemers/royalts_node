053d4ddcad17dd8612ff827853d8cd2f88ef6eab
Jeroen Steenkamp <j.steenkamp.@ing.nl>
2015-05-28 12:56:15 +0200

    creates requried folder xml, "xml = xml +" still ugly code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var builder = require('xmlbuilder');
 var uuid = require('node-uuid');
+var dateFormat = require('dateformat');
 
-function object(objectid,key,value) {
-    var xml = builder.create('KeyValuePairs')
+function xmlobject(objectid, key, value, options) {
+    var xml = builder.create('KeyValuePairs', options)
         .ele('ObjectID', objectid)
             .up()
         .ele('Key', key)
@@ -13,20 +14,39 @@ function object(objectid,key,value) {
 }
 
 module.exports = {
-    init: function(name) {
-        return object(uuid.v4(),'Name','Pfinbox');
-        var folderid = uuid.v4();
+    guid: function() {
+        return(uuid.v4());
+    },
+    init: function() {
+        var guid = uuid.v4();
         var xml = builder.create('NewDataSet')
-            .ele('repo', {'type': 'guid'}, folderid)
+            .ele('repo', {'type': 'guid'}, guid)
             .end({ pretty: true});
         return(xml);
     },
-    folder: function(name) {
-        var folderid = uuid.v4();
-        console.log("folder troep2, " + name);
+    folder: function(parentguid, name) {
+        //console.log("folder: " + name);
+        var guid = uuid.v4();
+        var date = dateFormat(new Date(), "yyyy/mm/dd H:MM:ss");
+        var xml = xmlobject(guid, 'RoyalObjectType', 'RoyalFolder');
+        var array = [
+            {name: 'ID', value: guid},
+            {name: 'Modified', value: date},
+            {name: 'Name', value: name},
+            {name: 'Created', value: date},
+            {name: 'IsExpanded', value: 'False'},
+            {name: 'CredentialMode', value: '3'},
+            {name: 'CredentialAutologon', value: 'True'},
+            {name: 'CredentialFromParent', value: 'True'},
+            {name: 'ParentID', value: parentguid}
+        ];
+        array.forEach(function(keypair) {
+            xml = xml + '\n' + xmlobject(guid, keypair['name'], keypair['value'], {headless: true});
+        });
+        return(xml);
     },
     terminal: function(name) {
         var terminalid = uuid.v4();
-        console.log("terminal troep, " + name);
+        console.log("terminal: " + name);
     }
 };
