bd5c058d4117cade345dfd1dc7a1417ad4dcf12c
Erik-jan Riemers <erik-jan.riemers@ing.nl>
2015-06-08 15:24:22 +0200

    Made it smarter, shorter. Added default objects config file.

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-var builder = require('xmlbuilder');
 var uuid = require('node-uuid');
 var moment = require('moment');
-
-function xmlobject(objectid, key, value, options) {
-    var xml = builder.create('KeyValuePairs', options)
-        .ele('ObjectID', objectid)
-            .up()
-        .ele('Key', key)
-            .up()
-        .ele('Value', value)
-        .end({pretty: true});
-    return (xml);
-}
+var js2xmlparser = require('js2xmlparser');
+var rtsd = require('./objectdefaults');
 
 module.exports = {
-    guid: function() {
-        return(uuid.v4());
-    },
-    init: function() {
-        var guid = uuid.v4();
-        var xml = builder.create('NewDataSet')
-            .ele('repo', {'type': 'guid'}, guid)
-            .end({ pretty: true});
-        return(xml);
-    },
-    folder: function(jsonObj, parentguid, name, farray) {
+    //guid: function() {
+    //    return(uuid.v4());
+    //},
+    object: function(parentguid, name, type) {
         var guid = uuid.v4();
         var date = moment().format('YYYY/mm/DD H:MM:ss');
+
+        var jsonStr ='{ "KeyValuePairs": [ ] }';
+        var jsonObj = JSON.parse(jsonStr);
         //should test if it actually is a valid json object
-        jsonObj.NewDataSet[0].KeyValuePairs.push ({ "ObjectID": guid, "Key": "RoyalObjectType", "Value": "RoyalFolder" });
+        jsonObj.KeyValuePairs.push ({ "ObjectID": guid, "Key": "RoyalObjectType", "Value": type });
         var array = [
             {name: 'ID', value: guid},
             {name: 'Modified', value: date},
@@ -36,17 +22,14 @@ module.exports = {
             {name: 'Created', value: date},
             {name: 'ParentID', value: parentguid}
         ];
-        var allarray = array.concat(farray);
+        var allarray = array.concat(rtsd[type]);
         //console.log(allarray);
         allarray.forEach(function(keypair) {
-            //xml = xml + '\n' + xmlobject(guid, keypair['name'], keypair['value'], {headless: true});
-            jsonObj.NewDataSet[0].KeyValuePairs.push ({ "ObjectID": guid, "Key": keypair['name'], "Value": keypair['value'] });
+            jsonObj.KeyValuePairs.push ({ "ObjectID": guid, "Key": keypair['name'], "Value": keypair['value'] });
         });
-        //return(xml);
         return(jsonObj);
     },
-    terminal: function(name) {
-        var terminalid = uuid.v4();
-        console.log("terminal: " + name);
+    generate: function(data) {
+        return(js2xmlparser("NewDataSet", data));
     }
 };
---
+++ objectdefaults.js
@@ -0,0 +1,26 @@
+var objectdefaults = {};
+
+objectdefaults.RoyalSSHConnection = [
+    {name: 'IsAdHoc', value: 'False'},
+    {name: 'IsConnectionTemplate', value: 'False'},
+    {name: 'InitialStateOfKeyboardScrolling', value: 'True'},
+    {name: 'CredentialFromParen', value: 'True'},
+    {name: 'HistoryMaxLength'}
+];
+
+objectdefaults.RoyalFolder = [
+    {name: 'IsExpanded', value: 'False'},
+    {name: 'CredentialMode', value: '3'},
+    {name: 'CredentialAutologon', value: 'True'},
+    {name: 'CredentialFromParent', value: 'True'}
+];
+
+objectdefaults.RoyalDocument = [
+    {name: 'DocumentType', value: 'Workspace'},
+    {name: 'CredentialMode', value: '3'},
+    {name: 'CredentialName', value: 'Unix'},
+    {name: 'CredentialAutoLogon', value: 'True'},
+    {name: 'CredentialMode', value: '4'}
+];
+
+module.exports = objectdefaults;
