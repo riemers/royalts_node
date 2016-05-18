b27c07841fa0f8d24884cc363216409ce8c65390
Erik-jan Riemers <erik-jan.riemers@ing.nl>
2016-05-18 22:33:24 +0200

    Update/Smaller footprint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # royalts_node
+
+This package can generate the GUID id's and comes with default objects per type too.
+
+Still old version layout, will be updated to latest soon.
--- index.js
+++ index.js
@@ -7,24 +7,15 @@ module.exports = {
     object: function(parentguid, name, type, cb) {
         var guid = uuid.v4();
         var date = moment().format('YYYY/mm/DD H:MM:ss');
-
-        var jsonStr ='{ "KeyValuePairs": [ ] }';
-        var jsonObj = JSON.parse(jsonStr);
-        //should test if it actually is a valid json object
-        jsonObj.KeyValuePairs.push ({ "ObjectID": guid, "Key": "RoyalObjectType", "Value": type });
-        var array = [
-            {name: 'ID', value: guid},
-            {name: 'Modified', value: date},
-            {name: 'Name', value: name},
-            {name: 'Created', value: date},
-            {name: 'ParentID', value: parentguid}
+        var array = [ 
+            { name: 'RoyalObjectType', value: type },
+            { name: 'ID', value: guid },
+            { name: 'Modified', value: date },
+            { name: 'Name', value: name },
+            { name: 'Created', value: date },
+            { name: 'ParentID', value: parentguid }
         ];
-        var allarray = array.concat(rtsd[type]);
-        allarray.forEach(function(keypair) {
-            jsonObj.KeyValuePairs.push ({ "ObjectID": guid, "Key": keypair['name'], "Value": keypair['value'] });
-        });
-
-        cb(jsonObj, guid);
+        cb(array.concat(rtsd[type]), guid);
 
     },
     generate: function(data) {
